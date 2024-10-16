851bcdc881c1e1ce948f6716915ebbf288a73d33
Ahsan Hanif <ahsan@timeahead.co>
2024-10-16 18:33:00 +0500

    Animation dynamic with textures

--- src/components/animation/Modal.tsx
+++ src/components/animation/Modal.tsx
@@ -1,20 +1,42 @@
-import { useEffect, useMemo, useRef, forwardRef, useImperativeHandle } from "react"
-import { useFrame } from "@react-three/fiber"
-import { Group, LoopOnce } from "three"
-import { useAnimations, useGLTF } from "@react-three/drei"
+import { useEffect, useMemo, useRef, forwardRef, useImperativeHandle, useState } from "react"
+import { useFrame, useThree } from "@react-three/fiber"
+import { Group, MeshStandardMaterial, LoopOnce, Vector3 } from "three"
+import { useAnimations, useGLTF, useTexture } from "@react-three/drei"
 
 const Modal = forwardRef((props, ref) => {
    const group = useRef<Group>(null)
-   const { nodes, materials, animations, scene } = useGLTF("/assets/animations/6_fig_yalla_v3.glb")
+   const { nodes, materials, animations, scene } = useGLTF("/assets/animations/6_fig_yalla_v4.glb")
+   const textures = useTexture([
+      "/assets/animations/textures/21.png",
+      "/assets/animations/textures/22.png",
+      "/assets/animations/textures/23.png",
+      "/assets/animations/textures/24.png",
+      "/assets/animations/textures/25.png",
+      "/assets/animations/textures/26.png",
+   ])
+   const materialNames = [
+      "rollout_ball.0",
+      "rollout_ball.001",
+      "rollout_ball.002",
+      "rollout_ball.003",
+      "rollout_ball.004",
+      "rollout_ball.005",
+   ]
+   const { camera } = useThree()
+   const [animationFinished, setAnimationFinished] = useState(false)
 
    const { actions } = useAnimations(animations, scene)
    const rotationAnim = actions["wheel_roll_1"]
    const ballOutAnimations = [
-      { action: actions["ball.2_roll_out"], name: "ball.2_roll_out" },
-      { action: actions["ball.3_roll_out.001"], name: "ball.3_roll_out.001" },
-      { action: actions["ball.4_roll_out"], name: "ball.4_roll_out" },
+      { action: actions["ball_1_roolout"], name: "out_1" },
+      { action: actions["ball.2_roll_out"], name: "out_2" },
+      { action: actions["ball.3_roll_out.001"], name: "out_3" },
+      { action: actions["ball.4_roll_out"], name: "out_4" },
+      { action: actions["Action.024"], name: "out_5" },
+      { action: actions["Action.025"], name: "out_6" },
    ]
    const ballAnimations = Array.from({ length: 25 }, (_, i) => actions[`ball_1st_anim_${i}`])
+   const targetPosition = new Vector3(-2, 0, 1)
 
    // Expose startAnimation method to parent component
    useImperativeHandle(ref, () => ({
@@ -48,6 +70,16 @@ const Modal = forwardRef((props, ref) => {
       console.log(actions)
       console.log(materials)
 
+      materialNames.forEach((materialName, index) => {
+         const material = materials[materialName] as MeshStandardMaterial | undefined
+         const texture = textures[index]
+   
+         if (material && texture) {
+           material.map = texture
+           material.needsUpdate = true
+         }
+      })
+
       return () => {
          if (rotationAnim) {
             rotationAnim.stop()
@@ -66,7 +98,7 @@ const Modal = forwardRef((props, ref) => {
             }
          })
       }
-   }, [actions, materials])
+   }, [actions, textures, materials])
 
    useFrame(() => {
       if (rotationAnim) {
@@ -75,6 +107,7 @@ const Modal = forwardRef((props, ref) => {
             console.log("Wheel animation has ended")
             rotationAnim.stop()
 
+            setAnimationFinished(true)
             ballOutAnimations.forEach(({ action, name }) => {
                if (action) {
                   action.clampWhenFinished = true
@@ -83,6 +116,19 @@ const Modal = forwardRef((props, ref) => {
                   console.log(`Animation '${name}' started`)
                }
             })
+         }
+      }
+
+      if (animationFinished) {
+         const currentPosition = camera.position.clone()
+         currentPosition.lerp(targetPosition, 0.05) // 0.05 is the interpolation factor
+         camera.position.copy(currentPosition)
+         camera.lookAt(0, 0, 0) // Keep the camera focused on the origin (or any target point)
+
+         // Check if camera has reached the target position
+         if (camera.position.distanceTo(targetPosition) < 0.01) {
+            console.log("Camera has reached the target position")
+            setAnimationFinished(false); // Reset the animation finished state if desired
          }
       }
    })
--- src/components/animation/Scene.tsx
+++ src/components/animation/Scene.tsx
@@ -13,7 +13,7 @@ function Loader() {
 export default function Scene({ modelRef }: any) {
    return (
       <div className="flex items-center justify-center flex-grow w-full h-full">
-         <Canvas camera={{ position: [-2, 1, 4] }} gl={{ antialias: true }} dpr={[1, 2]} className="relative h-svh">
+         <Canvas camera={{ position: [-2, 0, 4] }} gl={{ antialias: true }} dpr={[1, 2]} className="relative h-svh">
             <Suspense fallback={<Loader />}>
                <Environment files="/assets/animations/env.hdr" background backgroundBlurriness={0.2} />
                <Model ref={modelRef} />
