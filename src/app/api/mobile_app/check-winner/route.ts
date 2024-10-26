559c04ccddd3f16faeaa5383b6b81fd149b961e7
Adil1122 <adil7090@gmail.com>
2024-10-26 18:07:25 +0500

    check winner api change

---
+++ src/app/api/mobile_app/check-winner/route.ts
@@ -0,0 +1,81 @@
+import { NextResponse } from "next/server";
+import connectMongoDB from "@/libs/mongoosdb";
+import Invoice from "@/models/InvoiceModel";
+import Winner from "@/models/WinnerModel";
+
+export async function GET(request: any) {
+    try {
+        await connectMongoDB();
+        var url = new URL(request.url);
+        var searchparams = new URLSearchParams(url.searchParams);
+        var invoice_number = searchparams.get('invoice_number') + ''; 
+        var invoice = await Invoice.find({invoice_number: invoice_number}).select(['_id']).limit(1)
+        console.log('invoice: ', invoice[0]._id)
+        if(invoice && invoice.length > 0) {
+
+            const winner = await Winner
+            .aggregate([
+                {
+                    $match:
+                    {
+                        invoice_id: invoice[0]._id   
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'prizes',
+                        localField: "prize_id",
+                        foreignField: "_id",
+                        as: "winnerWithPrize",
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'games',
+                        localField: "game_id",
+                        foreignField: "_id",
+                        as: "winnerWithPrize",
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: "user_id",
+                        foreignField: "_id",
+                        as: "winnerWithUser",
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'products',
+                        localField: "product_id",
+                        foreignField: "_id",
+                        as: "winnerWithProduct",
+                    }
+                }
+            ]).sort({'winning_date': -1}).limit(1);
+
+            if(winner && winner.length > 0) {
+                return NextResponse.json({
+                    messge: "query success ....",
+                    winner: winner
+                  }, {status: 200});
+            } else {
+                return NextResponse.json({
+                    messge: "winner not found ....",
+                  }, {status: 500});
+            }
+        } else {
+            return NextResponse.json({
+                messge: "winner not found ....",
+              }, {status: 500});
+        }
+        
+
+    } catch (error) {
+        return NextResponse.json({
+            messge: "query error ....",
+            error: JSON.stringify(error)
+          }, {status: 500});
+    }
+}
