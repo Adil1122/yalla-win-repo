5db4d319c3cc9d3a6bebffc202fecc1ddea11e1b
Ahsan Hanif <ahsan@timeahead.co>
2024-11-01 01:32:02 +0500

    User report api data is dynamic

--- src/app/api/user/reports/route.ts
+++ src/app/api/user/reports/route.ts
@@ -1,84 +1,108 @@
 import { NextResponse } from "next/server";
+import Invoice from "@/models/InvoiceModel";
+import { getGraphResult, getStartEndDates } from "@/libs/common";
+import connectMongoDB from "@/libs/mongoosdb";
 
 export async function GET(request: any) {
 
-    var monthly: any = {};
-    for(var i = 1; i <= 30; i++) {
-        monthly['Day ' + i] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    }
+   await connectMongoDB()
+   var invoice_type: any = 'game'
 
-    try {
-        var data = 
-    {
-      "status": "success",
-      "data": {
-        "daily_report": [
-          {
-            "time_period": "8-10 AM",
-            "sales": 3
-          },
-          {
-            "time_period": "10-12 AM",
-            "sales": 4
-          },
-          {
-            "time_period": "12-2 PM",
-            "sales": 2
-          },
-          {
-            "time_period": "2-4 PM",
-            "sales": 5
-          },
-          {
-            "time_period": "4-6 PM",
-            "sales": 4
-          },
-          {
-            "time_period": "6-8 PM",
-            "sales": 2
-          }
-        ],
+   const dailyReport = await getReportBySchedule(invoice_type, 'daily')
+   const updatedDailyReport = transformDailyReport(dailyReport);
 
-        "weekly_report": [
-            {
-                "MON": 4,
-                "TUE": 10,
-                "WED": 2,
-                "THU": 5,
-                "FRI": 7,
-                "SAT": 3,
-                "SUN": 10,
-            }
-        ],
+   const monthlyReport = await getReportBySchedule(invoice_type, 'monthly')
+   const transformedMonthlyReport = transformMonthlyReport(monthlyReport)
+   
+   const weeklyReport = await getReportBySchedule(invoice_type, 'weekly')
+   const transformedWeeklyReport = transformWeeklyData(weeklyReport)
 
-        "sales_till_today": [
-            {
-                "JAN": 4,
-                "FEB": 10,
-                "MAR": 2,
-                "APR": 5,
-                "MAY": 7,
-                "JUN": 3,
-                "JUL": 10,
-                "AUG": 11,
-                "SEP": 4
-            }
-        ],
-
-        "monthly_report": [
-            monthly
-        ]
+   try {
+      var data =  {
+         "status": "success",
+         "data": {
+            "daily_report": updatedDailyReport,
+            "weekly_report": transformedWeeklyReport,
+            "sales_till_today": [
+               {
+                     "JAN": 4,
+                     "FEB": 10,
+                     "MAR": 2,
+                     "APR": 5,
+                     "MAY": 7,
+                     "JUN": 3,
+                     "JUL": 10,
+                     "AUG": 11,
+                     "SEP": 4
+               }
+            ],
+            "monthly_report": [
+               transformedMonthlyReport
+            ]
+         }
       }
-    }
-    return NextResponse.json({
-        messge: "query success ....",
-        data: data
-      }, {status: 200});
 
-    } catch (error) {
-        return NextResponse.json({
-            messge: "query error ....",
-          }, {status: 500});
-    }
-    
+      return NextResponse.json({
+         messge: "query success ....",
+         data: data
+      }, {status: 200})
+
+   } catch (error) {
+      return NextResponse.json({
+         messge: "query error ....",
+         }, {status: 500})
+   }
 }
+
+const getReportBySchedule = async (invoice_type: any, schedule: any) => {
+
+   var dates = getStartEndDates(schedule)
+   var start_date = dates.start_date
+   var end_date = dates.end_date
+
+   let queryConditions: any[] = [
+      {
+         createdAt: {
+            $gte: new Date(start_date), 
+            $lt: new Date(end_date)
+         }
+      },
+      { invoice_type: invoice_type }
+   ]
+
+   const records = await Invoice
+      .find({
+         $and: queryConditions
+      })
+      .sort({ 'createdAt': -1 })
+      .select(['_id', 'user_id', 'user_city', 'user_country', 'total_amount', 'invoice_date'])
+
+   return getGraphResult(records, start_date, end_date, schedule).data
+}
+
+const transformDailyReport = (dailyReport: any[]): any[] => {
+   return dailyReport.map((record: any) => ({
+     time_period: record.day,
+     sales: record.sales
+   }));
+ }
+
+ const transformMonthlyReport = (monthlyReport: any[]): any => {
+   return monthlyReport.reduce((acc: any, { day, sales }) => {
+     acc[`Day ${day}`] = sales
+     return acc
+   }, {})
+ }
+
+ const transformWeeklyData = (weeklyData: any[]): any[] => {
+   const salesByDay: any = {}
+ 
+   weeklyData.forEach((item) => {
+     const day = item.day
+     const sales = item.sales
+
+     salesByDay[day] = (salesByDay[day] || 0) + sales
+   })
+ 
+   return [salesByDay]
+ }
