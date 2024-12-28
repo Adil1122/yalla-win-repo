c6d2f588bf3a07ff385eb3ea3bd4694f3da381b8
Adil1122 <adil7090@gmail.com>
2024-12-28 12:24:44 +0500

    records object added in response

--- src/app/api/admin/dashboard/route.ts
+++ src/app/api/admin/dashboard/route.ts
@@ -68,6 +68,7 @@ export async function GET(request: Request) {
       return NextResponse.json({
          messge: "Query success ....",
          graph_result: getGraphResult(records, start_date, end_date, schedule),
+         records: records,
          merchants: merchants
       }, {status: 200});
 
